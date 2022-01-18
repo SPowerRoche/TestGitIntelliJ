70c044ed865cc59e10fc49ce3df4057fb300c631
SiobhanRoche <siobhanroche@wit.ie>
2022-01-18 15:18:56 +0000

    Added new line to progam

---
+++ src/TestClassGit.java
@@ -0,0 +1,6 @@
+public class TestClassGit {
+    public static void main(String args[]){
+        System.out.println("This is a test project for GitHub");
+        System.out.println("I added this line just to see what happens");
+    }
+}
